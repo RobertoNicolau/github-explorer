fb82b4e53579a657c205079660dca90d4d1ef7fc
Roberto <roberto.leonardo.2000@gmail.com>
2022-05-09 20:03:21 -0300

    Finalização do Github Explorer

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,32 +1,30 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = [{
-        name: "Unform",
-        description: "Formulários em React JS",
-        link: "https://github.com/unform/unform"
-    },
-    {
-        name: "Unform 2",
-        description: "Formulários em React JS",
-        link: "https://github.com/unform/unform"
-    },
-    {
-        name: "Unform",
-        description: "Formulários em React JS",
-        link: "https://github.com/unform/unform"
-    }]
+import '../styles/repositories.scss';
+
+//https://api.github.com/orgs/rocketseat/repos
 
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/robertonicolau/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return (
         <>
             <section className="repository-list">
                 <h1>Lista de repositórios</h1>
                 <ul>
-                    <RepositoryItem repository={repository[0]}/>
-                    <RepositoryItem repository={repository[1]}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
+                    {repositories.map(repository => {
+                        return (<RepositoryItem key={repository.name} repository={repository}/>)
+                    })}
                 </ul>
             </section>
         </>
